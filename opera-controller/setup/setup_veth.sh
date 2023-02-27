3995020ee04a1ae7309806a95534cc3c40136e2a
rukshani <rukshani@node1.8node.rotornet-pg0.utah.cloudlab.us>
2023-02-27 11:17:33 -0700

    set mtu

--- opera-controller/setup/setup_veth.sh
+++ opera-controller/setup/setup_veth.sh
@@ -11,6 +11,8 @@ ip_addr=$(ip -f inet addr show eno50np1 | awk '/inet / {print $2}')
 sudo ip netns exec blue ip addr add $ip_addr dev veth0
 sudo ip netns exec blue ip link set arp off dev veth0
 sudo ip netns exec blue ethtool -K veth0 tx off
+sudo ip netns exec blue ip link set veth0 mtu 3400
 
-
+sudo ip link set eno50np1 mtu 3490
+sudo ip link set veth1 mtu 3400
 
