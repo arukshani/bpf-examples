124e1bab1267361f08c1f6ce17be9de639a75eb9
rukshani <rukshani@node1.8node.rotornet-pg0.utah.cloudlab.us>
2023-02-27 10:01:51 -0700

    Update installation script

--- opera-controller/setup/install_dependencies.sh
+++ opera-controller/setup/install_dependencies.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-sudo apt-get update 
-sudo apt install clang llvm libelf-dev libpcap-dev gcc-multilib build-essential
-sudo apt install linux-tools-$(uname -r)
-sudo apt install linux-headers-$(uname -r)
-sudo apt install linux-tools-common linux-tools-generic
-sudo apt install tcpdump
-sudo apt install jq
-sudo apt-get install linuxptp
-sudo apt-get install libmnl-dev
-sudo apt install m4
+sudo apt-get -y update 
+sudo apt-get -y install clang llvm libelf-dev libpcap-dev gcc-multilib build-essential
+sudo apt-get -y install linux-tools-$(uname -r)
+sudo apt-get -y install linux-headers-$(uname -r)
+sudo apt-get -y install linux-tools-common linux-tools-generic
+sudo apt-get -y install tcpdump
+sudo apt-get -y install jq
+sudo apt-get -y install linuxptp
+sudo apt-get -y install libmnl-dev
+sudo apt-get -y install m4
