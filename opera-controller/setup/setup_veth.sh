0946c99695459f161c56fcfeedeb0d16dd819702
rukshani <rukshani@node-1.twointerface.rotornet-pg0.utah.cloudlab.us>
2023-03-12 11:12:34 -0600

    switch scenario

--- opera-controller/setup/setup_veth.sh
+++ opera-controller/setup/setup_veth.sh
@@ -6,13 +6,13 @@ sudo ip link set veth0 netns blue
 sudo ip netns exec blue ip link set dev veth0 up
 sudo ip link set dev veth1 up
 
-ip_addr=$(ip -f inet addr show eno50np1 | awk '/inet / {print $2}')
+ip_addr=$(ip -f inet addr show enp65s0f0np0 | awk '/inet / {print $2}')
 
 sudo ip netns exec blue ip addr add $ip_addr dev veth0
 sudo ip netns exec blue ip link set arp off dev veth0
 sudo ip netns exec blue ethtool -K veth0 tx off
 sudo ip netns exec blue ip link set veth0 mtu 3400
 
-sudo ip link set eno50np1 mtu 3490
+sudo ip link set enp65s0f0np0 mtu 3490
 sudo ip link set veth1 mtu 3400
 
