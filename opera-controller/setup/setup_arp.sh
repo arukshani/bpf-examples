974e849ed47306ee322254d339a4f111383e13a0
rukshani <rukshani@node-2.ruk.rotornet-pg0.utah.cloudlab.us>
2023-05-08 13:17:14 -0600

    arp commands

--- opera-controller/setup/setup_arp.sh
+++ opera-controller/setup/setup_arp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ip netns exec blue arp -s 192.168.1.2 02:10:30:0e:3b:ef
-sudo ip netns exec red arp -s 192.168.1.2 0e:7c:c4:3a:39:12 
-# sudo ip netns exec blue arp -s 192.168.1.1 ee:29:38:6f:21:7c
+# sudo ip netns exec blue arp -s 192.168.1.2 92:e3:3f:41:e4:e4
+# sudo ip netns exec blue arp -s 192.168.1.1 8a:ed:ff:d0:25:04
+# sudo ip netns exec red arp -s 192.168.1.2 0e:7c:c4:3a:39:12 
 # sudo ip netns exec red arp -s 192.168.1.1 d2:64:1a:75:9b:68
