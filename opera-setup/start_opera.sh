937a34d6e87de9dd5446b7ab059a3c755ea3efe0
rukshani <rukshani@node-33.opera1.rotornet-pg0.utah.cloudlab.us>
2023-05-25 14:12:09 -0600

    start command

--- opera-setup/start_opera.sh
+++ opera-setup/start_opera.sh
@@ -5,3 +5,4 @@
 # sudo ./opera_nic 192.168.1.1 configs/node-1-link.csv 60
 cd /opt/bpf-examples/opera-version1
 nohup sudo ./opera_nic $1 configs/$2.csv $3 120 > /dev/null 2>&1 &
+# nohup sudo ./opera_nic $1 configs/$2.csv $3 120 > /tmp/opera_nic.out 2>&1 &
