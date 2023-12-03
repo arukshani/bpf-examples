209e0ab9b293bab5d56ce01a7a0c8e4f8589afb6
rukshani <rukshani@node-1.tt.rotornet-pg0.clemson.cloudlab.us>
2023-12-03 01:26:30 +0000

    arp

--- opera-setup-cloudlab/add_arp.sh
+++ opera-setup-cloudlab/add_arp.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
 
-# echo $1 $2
-# sudo ip netns exec blue arp -i veth0 -s $1 $2
-# sudo ip netns exec red arp -i veth2 -s $1 $3
-
-num_namespaces=0
-node=$(hostname)
-# echo $node
-
-for arg in "$@"
-do
-case $arg in
-    -n|--number-of-ns)
-        shift
-        num_namespaces=$1
-        shift
-        ;;
-esac
-done
-
-myArray=("blue" "red" "ns12" "ns13" "ns15" "ns16" "ns17" "ns18" "ns19" "ns20" "ns21" "ns22" "ns23" "ns24")
-
-for i in $(seq 0 $num_namespaces); do
-    # ip=$(sudo ip netns exec ${myArray[$i]} ifconfig | awk '/inet / {print $2}')
-    # ether=$(sudo ip netns exec ${myArray[$i]} ifconfig | awk '/ether / {print $2}')
-    # echo $i, $node, ${myArray[$i]}, $ip, $ether >> /tmp/$node.csv
-done
+echo $1 $2 $3 $4
+# sudo ip netns exec $1 arp -i $2 -s $3 $4
